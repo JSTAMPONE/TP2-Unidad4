c70356ade9a0879b4ae7c9f6c36744b2867df8f9
Juan Ignacio Stampone <juanistampone10@gmail.com>
2026-05-25 18:59:42 +0000

    KAN-5: Desarrollo de cálculos de indicadores de ventas

--- scripts/analisis_ventas.py
+++ scripts/analisis_ventas.py
@@ -6,3 +6,24 @@ total_ventas = 0
 for venta in df["sales_amount"]:
     total_ventas += venta
 print(f"Ventas totales: {total_ventas}")
+print("---")
+ventas_por_mes = {}
+for i in range(len(df)):
+    mes = df["sales_date"][i].split("-")[1]
+    monto = df["sales_amount"][i]
+    if mes in ventas_por_mes:
+        ventas_por_mes[mes] += monto
+    else:
+        ventas_por_mes[mes] = monto
+print("Ventas por mes:")
+for mes, total in ventas_por_mes.items():
+    print(f"Mes {mes} : {int(total)}")
+print("---")
+mes_mayor = None
+monto_mayor = 0
+for mes, total in ventas_por_mes.items():
+    if int(total) > monto_mayor:
+        monto_mayor = int(total)
+        mes_mayor = mes
+
+print(f"Mes con mayor venta: Mes {mes_mayor} con {monto_mayor} ventas.")
